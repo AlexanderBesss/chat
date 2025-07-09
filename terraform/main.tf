a8f388439e6442a4f54fecbfb58dc838b6b5035c
Oleksandr Besarab <alexandrbesss@gmail.com>
2025-07-09 09:34:49 +0300

    fix ports

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "ecs" {
   vpc_id      = data.aws_vpc.default.id
 
   egress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
