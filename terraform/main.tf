d4ed13e0b7fb9f50825490fa161cf209ed52f5d7
Oleksandr Besarab <alexandrbesss@gmail.com>
2025-07-09 09:46:57 +0300

    add ecr repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -175,3 +175,39 @@ resource "aws_security_group" "mysql" {
     Name = "MySQL SG"
   }
 }
+
+resource "aws_ecr_repository" "chat_app" {
+  name = "test/chat-api"
+
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Project     = "Chat App"
+    Environment = "Test"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "chat_app_policy" {
+  repository = aws_ecr_repository.chat_app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only latest 2 images"
+        selection = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 2
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
