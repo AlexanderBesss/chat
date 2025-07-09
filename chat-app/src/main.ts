0f417d893c356e28c04a80d0c25b52d3144d2304
Oleksandr Besarab <alexandrbesss@gmail.com>
2025-07-09 10:09:03 +0300

    log port

--- chat-app/src/main.ts
+++ chat-app/src/main.ts
@@ -5,6 +5,7 @@ import { ENV, validateEnv } from './env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log('Starting app om port: ', ENV.APP_PORT);
   await app.listen(ENV.APP_PORT, '0.0.0.0');
 }
 validateEnv();
